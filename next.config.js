1e86800eec891708f4f37363c4cd355c8e1b0463
Evgeny Simonov <workjett@gmail.com>
2022-05-31 09:59:23 +0300

    add back server

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'http://localhost:8000/api/v1/:path*',
+        destination: 'http://923039-cs31212.tmweb.ru:8000/api/v1/:path*',
       }
     ]
   }
