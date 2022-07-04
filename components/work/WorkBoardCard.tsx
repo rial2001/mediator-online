b3caeabd4fe45c1c5133539cb1671fafd89f619a
Evgeny Simonov <workjett@gmail.com>
2022-07-04 11:53:28 +0300

    resolve category

--- components/work/WorkBoardCard.tsx
+++ components/work/WorkBoardCard.tsx
@@ -5,11 +5,38 @@ import WorkBoardMember from '@components/work/WorkBoardMember';
 import { appRouters } from '@routers/appRouters';
 
 import styles from '@styles/work/WorkBoardCardStyle.module.css';
+import categories from '@configs/categories.json';
+
+interface CategoryItem {
+  title: string;
+  value: string;
+  items?: Array<CategoryItem>;
+}
+
+const findCategory = (category: string): string | null => {
+  const processItem = (cat: CategoryItem): string | null => {
+    if(cat.items) {
+      for(const item of cat.items) {
+        const item_res = processItem(item)
+        if(item_res != null) return item_res
+      }
+    } else if(cat.value == category) {
+      return cat.title
+    }
+  }
+
+  const list: Array<CategoryItem> = categories
+  for(const item of list) {
+    const item_res = processItem(item)
+    if(item_res != null) return item_res
+  }
+  return null
+}
 
 const WorkBoardCard = ({ ...someWork }) => (
   <div className={styles.workBoardCard}>
     <Typography.Title className={styles.cardTitle} level={3}>
-      {someWork.category}
+      {findCategory(someWork.category)}
     </Typography.Title>
     <Typography.Paragraph className={styles.cardDescription}>
       {someWork.description}
